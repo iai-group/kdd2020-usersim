5cc7285bea172c6b6b6dff9f037d31d24c62ff31
Shuo Zhang <imsure318@163.com>
2020-06-28 11:38:27 +0100

    ..
    ..

--- code/dialog/conv_man.py
+++ code/dialog/conv_man.py
@@ -5,20 +5,22 @@ ConversationManager
 Author: Shuo Zhang, Krisztian Balog
 """
 
-from code.user.human_user import HumanUser
-# from code.bot.movie_bot import MovieBot
-from code.bot.and_chill import AndChill, Botbot
-from code.telegram.balog_bot import *
-import json
-import time
 from code.bot.jmrs1.ConversationalAgent.ConversationalSingleAgent import \
     ConversationalSingleAgent
+from code.nlp.movies.movies_nlg import RESPONSE_TEMPLATES_MS, RESPONSE_TEMPLATES_MB, RESPONSE_TEMPLATES_AC
+from code.user.movies.movies_simulated_user import MovieSimulatedUser
+from code.user.human_user import HumanUser
+from code.bot.and_chill import Botbot
+from code.telegram.balog_bot import *
+import json
 import configparser
 import yaml
 import sys
 import os.path
 import time
 import random
+
+USER_TAG, AGENT_TAG = "user", "agent"
 
 
 class ConversationManager:
@@ -31,109 +33,7 @@ class ConversationManager:
         self._bb = BalogBot()
         self._chat = None
 
-    def moviebot_to_teelgram(self):
-        last_update_id = None
-        while True:
-            updates = self._bb.get_updates(last_update_id)
-            if len(updates["result"]) > 0:
-                for update in updates["result"]:
-                    print(last_update_id, update["message"]["text"])
-                    flag = False
-                    if update["message"]["text"].lower() == "stop":
-                        flag = True
-                last_update_id = self._bb.get_last_update_id(updates) + 1
-                self.handle_updates_moviebot(updates)
-                if flag:
-                    break
-            time.sleep(0.5)
-
-    def handle_updates_moviebot(self, updates):
-        for update in updates["result"]:
-            text = update["message"]["text"]
-            chat = update["message"]["chat"]["id"]
-            self._chat = chat
-            self._history = [i for i in self._history if i[2] == self._chat]
-            if text == "/done":
-                self._bb.send_message("Select an item to delete", chat, keyboard)
-            elif text == "/start" or text.lower() == "hello" or text.lower() == "hi" or text.lower() == "go":
-                self._history.append(["user", "hello", chat])
-                system_utterance = self._bot.generate_response("hello")
-                self._bb.send_message(system_utterance, chat)
-                self._history.append(["agent", system_utterance, chat])
-            elif text.lower() == "code":
-                if len(self._history) < 8:
-                    self._bb.send_message("You have not started your chat!", chat)
-                else:
-                    self._bb.send_message("$%GVN&K<)", chat)
-            elif text.startswith("/"):
-                continue
-            elif text in items:
-                self._history.append(["user", text, chat])
-                system_utterance = self._bot.generate_response(text)
-                self._bb.send_message(system_utterance, chat)
-                self._history.append(["agent", system_utterance, chat])
-            else:
-                self._history.append(["user", text, chat])
-                system_utterance = self._bot.generate_response(text)
-                self._bb.send_message(system_utterance, chat)
-                self._history.append(["agent", system_utterance, chat])
-
-    def andchill_to_teelgram(self):
-        last_update_id = None
-        while True:
-            updates = self._bb.get_updates(last_update_id)
-            try:
-                _, system_utterance, chat = self._history[-1]
-                a = self._bot.fetch_last_msg(system_utterance)
-            except Exception:
-                a = None
-            if a:
-                for item in a:
-                    self._bb.send_message(item, chat)
-                    self._history.append(["agent", item, chat])
-            if len(updates["result"]) > 0:
-                last_update_id = self._bb.get_last_update_id(updates) + 1
-                self.handle_updates_andchill(updates)
-            time.sleep(0.5)
-            if self._chat:
-                with open(str(self._chat) + ".json", "w", encoding="utf-8") as f:
-                    json.dump(self._history, f, indent=2)
-
-    def handle_updates_andchill(self, updates):
-        for update in updates["result"]:
-            text = update["message"]["text"]
-            chat = update["message"]["chat"]["id"]
-            self._chat = chat
-            self._history = [i for i in self._history if i[2] == self._chat]
-            print(text)
-            if text == "/done":
-                print(self._history)
-                # keyboard = self._bb.build_keyboard(items)
-                # self._bb.send_message("Select an item to delete", chat, keyboard)
-            elif text.lower() == "code":
-                if len(self._history) < 8:
-                    self._bb.send_message("You have not started your chat!", chat)
-                else:
-                    self._bb.send_message("$%GVN&K<)" + str(chat), chat)
-            elif text == "/start" or text.lower() == "go":
-                self._history.append(["user", "go", chat])
-                system_utterance = self._bot.generate_response("go")
-                self._history.append(["agent", system_utterance, chat])
-                self._bb.send_message(system_utterance, chat)
-            elif text.startswith("/"):
-                continue
-            elif text in items:
-                self._history.append(["user", text, chat])
-                system_utterance = self._bot.generate_response(text)
-                self._history.append(["agent", system_utterance, chat])
-                self._bb.send_message(system_utterance, chat)
-            else:
-                self._history.append(["user", text, chat])
-                system_utterance = self._bot.generate_response(text)
-                self._history.append(["agent", system_utterance, chat])
-                self._bb.send_message(system_utterance, chat)
-
-    def run_single_agent(self, config, num_dialogues):
+    def run_single_agent(self, config):
         """
         This function will create an agent and orchestrate the conversation.
 
@@ -175,6 +75,50 @@ class ConversationManager:
             if self._chat:
                 with open(str(self._chat)+".json", "w", encoding="utf-8") as f:
                     json.dump(self._history, f, indent=2)
+
+    def run_ms(self, config, age_mode="our", persona=True):
+        """Let the manager run and monitor the conversations!"""
+        result = {
+            "dialog": []
+        }
+        ca = ConversationalSingleAgent(config)
+        ca.initialize()
+        if age_mode == "qfra":
+            self._user.init_agenda_qrfa()
+        elif age_mode == "qrfa_test":
+            self._user.init_agenda_qrfa_test()
+        elif age_mode == "our":
+            self._user.init_agenda()
+        else:
+            raise TypeError("Set age mode 'qfra', 'qrfa_test', or 'our'")
+        result["agenda"] = list(self._user.agenda)
+        user_utterance = self._user.init_dialog()
+        self._history.append(user_utterance)
+        result["dialog"].append([USER_TAG, "Hello", "Non-disclose"])
+        while True:
+            print("USER: {}.   [{}]".format(user_utterance, self._user._current_intent))
+            if user_utterance and user_utterance.lower() == "hi" or user_utterance.lower() == "hello" or user_utterance.lower() == "go":
+                system_utterance = ca.start_dialogue()
+                sys_intent = ["Elicit"]
+            elif user_utterance.lower() == "stop":
+                break
+            else:
+                system_utterance = ca.continue_dialogue(user_utterance)
+
+            time.sleep(1)
+            user_utterance, sys_intent = self._user.generate_response(system_utterance, persona=persona)
+            print("System: {}.    [{}]".format(system_utterance, sys_intent))
+            result["dialog"].append([AGENT_TAG, system_utterance, sys_intent[0]])
+            print("-----------------------------------------------------------")
+            if not user_utterance:
+                print("ALERT")
+            self._history.append(user_utterance)
+            result["dialog"].append([USER_TAG, user_utterance, self._user._current_intent])
+        result["persona"] = {"persona": self._user.persona, "preferences": self._user.preferences}
+        from pprint import pprint
+        pprint(result)
+        return result
+
 
 def arg_parse(args=None):
     """
@@ -232,12 +176,26 @@ def arg_parse(args=None):
             'interaction_mode': interaction_mode}
 
 if __name__ == "__main__":
-    user = HumanUser()  # change here to use a simulated user
-    bot = Botbot()
-    conv_man = ConversationManager(user, bot)
-    args = ['','-config', os.path.join(os.path.abspath("./.."), "kdd2020-userSim/code/bot/jmrs1/config/movies_text.yaml")]
-    args = arg_parse(args)
-    cfg_parser = args['cfg_parser']
-    dialogues = args['dialogues']
-    interaction_mode = args['interaction_mode']
-    conv_man.run_single_agent(cfg_parser, dialogues)
+    mode = "manual1"
+
+    if mode == "manual":
+        user = HumanUser()  # change here to use a simulated user
+        bot = Botbot()
+        conv_man = ConversationManager(user, bot)
+        args = ['','-config', os.path.join(os.path.abspath("./.."), "kdd2020-userSim/code/bot/jmrs1/config/movies_text.yaml")]
+        args = arg_parse(args)
+        cfg_parser = args['cfg_parser']
+        dialogues = args['dialogues']
+        interaction_mode = args['interaction_mode']
+        conv_man.run_single_agent(cfg_parser)
+    else:
+        hist = []
+        user = MovieSimulatedUser("code/data/1224_ms.json", RESPONSE_TEMPLATES_MS, "ms")  # change here to use a simulated user
+        bot = Botbot()
+        conv_man = ConversationManager(user, bot)
+        args = ['','-config', os.path.join(os.path.abspath("./.."), "kdd2020-userSim/code/bot/jmrs1/config/movies_text.yaml")]
+        args = arg_parse(args)
+        cfg_parser = args['cfg_parser']
+        interaction_mode = args['interaction_mode']
+        res = conv_man.run_ms(cfg_parser, age_mode="our", persona=True)
+        hist.append(res)
